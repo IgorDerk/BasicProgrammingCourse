9afa5445727faaa896518bfacb63401ba6d3f9bd
IgorDerk <derk.i26252@gmail.com>
2024-09-11 13:57:17 +0200

    Initial commit

---
+++ src/lesson_01/Hello.java
@@ -0,0 +1,14 @@
+package lesson_01;
+
+public class Hello { // Обьявляем класс Hello
+    // Здесь мы будем писать наш код
+
+    // psvm
+    // main
+    public static void main(String[] args) { // Обьявляем метод  main
+    // Код, который мы хотим выполнить
+        // sout
+
+        System.out.println("Hello, World!!!"); // Это инструкция/команда, которая выводит текст на экран
+    }
+}
