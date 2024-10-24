99cc28ef14794ab6045c0308dca548745741a8c7
IgorDerk <derk.i26252@gmail.com>
2024-10-24 15:22:40 +0200

    homework_31

---
+++ src/homework_31/task_01/Seasons.java
@@ -0,0 +1,37 @@
+package homework_31.task_01;
+
+/*
+Task 1
+Реализуйте Enum для определения четырех времен года с методами, возвращающими среднюю температуру для каждого времени года.
+ */
+public enum Seasons {
+
+    WINTER("Зима", 3),
+    SPRING("Весна", 12),
+    SUMMER("Лето", 20),
+    FALL("Осень", 14);
+
+    private String russianName;
+    private int averageTempSeasons;
+
+    Seasons(String russian, int temp) {
+        this.russianName = russian;
+        this.averageTempSeasons = temp;
+    }
+
+
+    public int getAverageTempSeasons() {
+        return averageTempSeasons;
+    }
+
+    public String getRussianName() {
+        return russianName;
+    }
+
+    @Override
+    public String toString() {
+        return "Сезон: " + this.russianName +
+                ", средняя температура: " + this.averageTempSeasons + "°C";
+    }
+}
+
---
+++ src/homework_31/task_01/SeasonsApp.java
@@ -0,0 +1,11 @@
+package homework_31.task_01;
+
+public class SeasonsApp {
+    public static void main(String[] args) {
+        for(Seasons seasons : Seasons.values()){
+            System.out.println(seasons);
+        }
+
+    }
+
+}
